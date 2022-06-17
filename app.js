76c459c330c81e865188d458563e4821e71691c7
Luis Rosales <luismrosales2@gmail.com>
2022-06-17 15:17:34 -0400

    added functionality to sliders

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // Global selections and variables
 const colorDivs = document.querySelectorAll(".color");
 const generateBtn = document.querySelector(".generate");
-const sliders = document.querySelector('input[type="range"]');
+const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
 let initialColors;
+
+// Event listeners
+sliders.forEach((slider) => {
+  slider.addEventListener("input", hslControls);
+});
 
 // Functions
 
@@ -60,4 +65,26 @@ function colorizeSliders(color, hue, brightness, saturation) {
   )},${scaleBrightness(0.5)},${scaleBrightness(1)})`;
   hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75), rgb(75,204,75), rgb(75,204,204), rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
 }
+
+function hslControls(e) {
+  const index =
+    e.target.getAttribute("data-bright") ||
+    e.target.getAttribute("data-sat") ||
+    e.target.getAttribute("data-hue");
+
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+  const hue = sliders[0];
+  const brightness = sliders[1];
+  const saturation = sliders[2];
+  const bgColor = colorDivs[index].querySelector("h2").innerText;
+
+  let color = chroma(bgColor)
+    .set("hsl.s", saturation.value)
+    .set("hsl.l", brightness.value)
+    .set("hsl.h", hue.value);
+
+  colorDivs[index].style.backgroundColor = color;
+}
+
+// function calls
 randomColors();
