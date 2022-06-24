eb6e1891a4ed4d3df5de54174007c8900a267228
Luis Rosales <luismrosales2@gmail.com>
2022-06-24 10:43:48 -0400

    fixed bugs and local storage

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Randm Color Pallete Generator
 
+This desktop website helps you pick from hundreds of random colors and allows you to save them.
+
 ## Live page: https://luismrosales.github.io/random-color/
--- app.js
+++ app.js
@@ -245,7 +245,13 @@ function savePalette(e) {
     colors.push(hex.innerText);
   });
   // generate object
-  let paletteNbr = savedPalettes.length;
+  let paletteNbr;
+  const paletteObjects = JSON.parse(localStorage.getItem("palettes"));
+  if (paletteObjects) {
+    paletteNbr = paletteObjects.length;
+  } else {
+    paletteNbr = savedPalettes.length;
+  }
   const paletteObj = { name, colors, nr: paletteNbr };
   savedPalettes.push(paletteObj);
   // save to local storage
@@ -280,7 +286,7 @@ function savePalette(e) {
       checkTextContrast(color, text);
       updateTextUi(index);
     });
-    libraryInputUPdate();
+    resetInputs();
   });
   // append to library
   palette.appendChild(title);
@@ -311,7 +317,53 @@ function closeLibrary() {
   popup.classList.remove("active");
 }
 
-function libraryInputUPdate() {}
+function getLocal() {
+  if (localStorage.getItem("palettes") === null) {
+    localPalettes = [];
+  } else {
+    const paletteObjects = JSON.parse(localStorage.getItem("palettes"));
+    savedPalettes = [...paletteObjects];
 
+    paletteObjects.forEach((paletteObj) => {
+      // generate the palette for the library
+      const palette = document.createElement("div");
+      palette.classList.add("custom-palette");
+      const title = document.createElement("h4");
+      title.innerText = paletteObj.name;
+      const preview = document.createElement("div");
+      preview.classList.add("small-preview");
+      paletteObj.colors.forEach((smallColor) => {
+        const smallDiv = document.createElement("div");
+        smallDiv.style.backgroundColor = smallColor;
+        preview.appendChild(smallDiv);
+      });
+      const paletteBtn = document.createElement("button");
+      paletteBtn.classList.add("pick-palette-btn");
+      paletteBtn.classList.add(paletteObj.nr);
+      paletteBtn.innerText = "Select";
+
+      // Attatch events to the library button
+      palette.addEventListener("click", (e) => {
+        closeLibrary();
+        const paletteIndex = e.target.classList[1];
+        initialColors = [];
+        paletteObjects[paletteIndex].colors.forEach((color, index) => {
+          initialColors.push(color);
+          colorDivs[index].style.backgroundColor = color;
+          const text = colorDivs[index].children[0];
+          checkTextContrast(color, text);
+          updateTextUi(index);
+        });
+        resetInputs();
+      });
+      // append to library
+      palette.appendChild(title);
+      palette.appendChild(preview);
+      palette.appendChild(paletteBtn);
+      libraryContainer.children[0].appendChild(palette);
+    });
+  }
+}
 // function calls
+getLocal();
 randomColors();
